c14e5b971a7b2aeec82fad951a8839219c0b59e7
Guilherme Freire <gfreire@MB.local>
2023-03-06 17:56:17 -0300

    Providing context

--- app/src/main/java/com/example/mvcpuredi/AppCompositionRoot.kt
+++ app/src/main/java/com/example/mvcpuredi/AppCompositionRoot.kt
@@ -1,5 +1,6 @@
 package com.example.mvcpuredi
 
+import android.app.Application
 import androidx.annotation.UiThread
 import com.example.mvcpuredi.networking.UsersApi
 import okhttp3.OkHttpClient
@@ -8,7 +9,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @UiThread
-class AppCompositionRoot {
+class AppCompositionRoot(val application: Application) {
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder().baseUrl("http://10.0.2.2:8000/")
             .addConverterFactory(GsonConverterFactory.create())
