81b9421899e35ffb0e118ca8b01aa3c871cbaf18
Guilherme Freire <gfreire@MB.local>
2023-03-07 20:38:35 -0300

    Multi module presentation component

--- app/src/main/java/com/example/mvcpuredi/di/presentation/PresentationComponent.kt
+++ app/src/main/java/com/example/mvcpuredi/di/presentation/PresentationComponent.kt
@@ -5,7 +5,7 @@ import com.example.mvcpuredi.screens.UserListFragment
 import dagger.Subcomponent
 
 @PresentationScope
-@Subcomponent(modules = [PresentationModule::class])
+@Subcomponent(modules = [PresentationModule::class, UseCasesModule::class])
 interface PresentationComponent {
     fun inject(fragment: UserListFragment)
     fun inject(activity: UserDetailActivity)
